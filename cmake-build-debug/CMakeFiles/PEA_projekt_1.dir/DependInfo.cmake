8f571e852484710764948f9913e27da0cb9da962
Tesa44 <mateusz.tesarewicz123@gmail.com>
2024-10-10 09:44:58 +0200

    changed cmake version to 3.27

---
+++ cmake-build-debug/CMakeFiles/PEA_projekt_1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Mateusz/CLionProjects/PEA-projekt-1/Menu.cpp" "CMakeFiles/PEA_projekt_1.dir/Menu.cpp.obj" "gcc" "CMakeFiles/PEA_projekt_1.dir/Menu.cpp.obj.d"
+  "C:/Users/Mateusz/CLionProjects/PEA-projekt-1/main.cpp" "CMakeFiles/PEA_projekt_1.dir/main.cpp.obj" "gcc" "CMakeFiles/PEA_projekt_1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PEA_projekt_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PEA_projekt_1.dir/Menu.cpp.obj"
+  "CMakeFiles/PEA_projekt_1.dir/Menu.cpp.obj.d"
+  "CMakeFiles/PEA_projekt_1.dir/main.cpp.obj"
+  "CMakeFiles/PEA_projekt_1.dir/main.cpp.obj.d"
+  "PEA_projekt_1.exe"
+  "PEA_projekt_1.exe.manifest"
+  "PEA_projekt_1.pdb"
+  "libPEA_projekt_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PEA_projekt_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
