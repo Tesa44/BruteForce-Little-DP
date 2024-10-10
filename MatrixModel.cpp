a353fcad5c366897c2fa7f26b5e3325abefc668a
Tesa44 <mateusz.tesarewicz123@gmail.com>
2024-10-10 14:05:50 +0200

    added matrix class

---
+++ MatrixModel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mateusz on 10.10.2024.
+//
+
+#include "matrixModel.h"
---
+++ MatrixModel.h
@@ -0,0 +1,14 @@
+//
+// Created by Mateusz on 10.10.2024.
+//
+
+#ifndef PEA_PROJEKT_1_MATRIXMODEL_H
+#define PEA_PROJEKT_1_MATRIXMODEL_H
+
+
+class matrixModel {
+
+};
+
+
+#endif //PEA_PROJEKT_1_MATRIXMODEL_H
